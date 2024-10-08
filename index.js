54b56e53954cac91ce0ee81eaf6c6bffbe93c325
DevanshuSawarkar <sawarkardevanshu@gmail.com>
2024-10-08 15:38:47 +0530

    Adding Code.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log(Math.ceil(Math.random()*100))
+let userInEl = document.getElementById("userIn");
+let randomNum = Math.ceil(Math.random() * 100);
+
+function onChangeUserIn() {
+    let userVal = parseInt(userInEl.value);
+    
+    if(userVal > randomNum) {
+        console.log("Its too high!!!");
+    } else if(userVal < randomNum) {
+        console.log("Its too low!!!");
+    } else {
+        console.log("You Won!!!");
+    }
+}
